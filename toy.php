b38e36f7bcbea6a98fbf946e19a6b073ed4a0af1
OMJI SHUKLA <91083571+009os@users.noreply.github.com>
2023-06-01 11:43:05 +0530

    Add files via upload

---
+++ toy.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>toys</title>
+</head>
+
+<body style="background-image: url('backSHOP.jpg');
+    width:100%;
+ background-position: center;
+ background-size: cover;
+background-repeat: no-repeat">
+    <center>
+        <header>
+            <h1><b><i>TOYS COLLECTION</i></b></h1>
+        </header>
+    </center>
+    <hr>
+    <center>
+        <table class="cal2">
+            <tr>
+                <td><a href="" target="_main"><img class="o11" src="toy1.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>HORSE TOY<br>BUT AT INR :- 999<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="toy2.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>TOY STORY<br>BUT AT INR :- 899<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="toy3.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>TRUCK<br>BUT AT INR :- 499<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+
+            </tr>
+            <tr>
+                <td><a href="" target="_main"><img class="o11" src="toy4.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>CAR<br>BUT AT INR :- 999<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="toy5.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>TEDDY<br>BUT AT INR :- 1199<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="toy6.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>BATMAN SUPERCAR<br>BUT AT INR :- 499<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+
+            </tr>
+
+        </table>
+    </center>
+    <footer>@omjishukla</footer>
+
+</body>
+
+
+</books
